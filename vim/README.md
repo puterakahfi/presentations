1809ecb1ecee448b630b7b7e517aad1852dc8f0d
puterakahfi <puterakahfi@gmail.com>
2017-01-11 11:12:58 +0700

    remove old impress

--- vim/README.md
+++ vim/README.md
@@ -1,7 +1,7 @@
 impress.js
 ============
 
-[![CircleCI](https://circleci.com/gh/impress/impress.js.svg?style=svg)](https://circleci.com/gh/impress/impress.js)
+[![CircleCI](https://circleci.com/gh/henrikingo/impress.js.svg?style=svg)](https://circleci.com/gh/henrikingo/impress.js)
 
 It's a presentation framework based on the power of CSS3 transforms and
 transitions in modern browsers and inspired by the idea behind prezi.com.
@@ -14,21 +14,17 @@ impress.js may not help you if you have nothing interesting to say ;)
 HOW TO USE IT
 ---------------
 
-[Use the source](index.html), Luke ;)
+Reference documentation of all impress.js features and API you can find it in [DOCUMENTATION.md](DOCUMENTATION.md).
 
-The [HTML source code](index.html) serves as a good example usage and contains comments explaning various features of impress.js. For more information about styling you can look into [CSS code](css/impress-demo.css) which shows how classes provided by impress.js can be used. Last but not least [JavaScript code of impress.js](js/impress.js) has some useful comments if you are interested in how everything works. Feel free to explore!
-
-If you want more straightforward reference documentation of all impress.js features and API you can find it in [DOCUMENTATION.md](DOCUMENTATION.md).
-
-
-EXAMPLES AND OTHER LEARNING RESOURCES
----------------------------------------
+The [HTML source code](index.html) of the official [impress.js demo](http://henrikingo.github.io/impress.js/) serves as a good example usage and contains comments explaning various features of impress.js. For more information about styling you can look into [CSS code](css/impress-demo.css) which shows how classes provided by impress.js can be used. Last but not least [JavaScript code of impress.js](js/impress.js) has some useful comments if you are interested in how everything works. Feel free to explore!
 
 ### Official demo
 
-[impress.js demo](http://impress.github.io/impress.js/) by [@bartaz](http://twitter.com/bartaz)
+[impress.js demo](http://henrikingo.github.io/impress.js/) by [@bartaz](http://twitter.com/bartaz)
 
 ### Examples and demos
+
+The [Classic Slides](http://henrikingo.github.io/impress.js/examples/classic-slides/) demo is targeted towards beginners, or can be used as a template for presentations that look like the traditional PowerPoint slide deck.
 
 More examples and demos can be found on [Examples and demos wiki page](http://github.com/impress/impress.js/wiki/Examples-and-demos).
 
@@ -42,13 +38,58 @@ on the wiki, too.
 There is also a book available about [Building impressive presentations with impress.js](http://www.packtpub.com/building-impressive-presentations-with-impressjs/book) by Rakhitha Nimesh Ratnayake.
 
 
+REPOSITORY STRUCTURE
+--------------------
+
+* [index.html](index.html): This is the official impress.js demo, showcasing all of the features 
+  of the original impress.js, as well as some new plugins as we add them.
+  * As already mentioned, this file is well commented and acts as the canonical documentation.
+* [examples/](examples/): Contains [another demo](examples/classic-slides/index.html)
+  that you can use as template for your first simple slide shows.
+* [src/](src/): The main file is [src/impress.js](src/impress.js). Additional
+  functionality is implemented as plugins in [src/plugins/](src/plugins/).
+  * See [src/plugins/README.md](src/plugins/README.md) for information about
+    the plugin API and how to write plugins.
+* [test/](test/): Contains QUnit and Syn libraries that we use for writing tests,
+  as well as some test coverage for core functionality. (Yes, more tests are
+  much welcome.) Tests for plugins are in the directory of each plugin.
+* [js/](js/): Contains [js/impress.js](js/impress.js), which contains a 
+  concatenation of the core `src/impress.js` and all the plugins. Traditionally
+  this is the file that you'll link to in a browser. In fact both the demo and
+  test files do exactly that.
+* [css/](css/]: Contains a CSS file used by the demo. This file is 
+  **not required for using impress.js** in your own presentations. Impress.js
+  creates the CSS it needs dynamically.
+* [extras/](extras/) contains plugins that for various reasons aren't
+  enabled by default. You have to explicitly add them with their own `script`
+  element to use them.
+* [build.js](build.js): Simple build file that creates `js/impress.js`. It also
+  creates a minified version `impress.min.js`, but that one is not included in the
+  github repository.
+* [package.json](build.js): An NPM package specification. This was mainly added
+  so you can easily install [buildify](https://www.npmjs.com/package/buildify)
+  and run `node build.js`. Other than the build process (which is really just
+  doing roughly `cat src/impress.js src/plugins/*/*.js > js/impress.js`) 
+  `impress.js` itself doesn't depend on Node or any NPM modules.
+* [bower.json](bower.json): A Bower package file. We also don't depend on Bower,
+  but provide this file if you want to use it.
+
 WANT TO CONTRIBUTE?
 ---------------------
 
-Please, read the [contributing guidelines](.github/CONTRIBUTING.md) on how to create [Issues](.github/CONTRIBUTING.md#issues) and [Pull Requests](.github/CONTRIBUTING.md#pull-requests).
+For developers, once you've made changes to the code, you should run these commands for testing:
 
-**Note:** The team has changed, so there will be many changes in the upcoming versions.
-If you need informations about versions, check the [changelog](CHANGELOG.md).
+    npm run build
+    npm run test
+    npm run lint
+
+Note that running `firefox qunit_test_runner.html` is usually more informative than running `karma` with `npm run test`. They both run the same tests.
+
+More info about the [src/](src/) directory can be found in [src/plugins/README.md](src/plugins/README.md).
+
+[Upstream impress.js](https://github.com/impress/impress.js/) has its own bureacracy around contributing. I welcome pull requests against this fork as well, just submit a PR on github and I'll be happy to look at it :-)
+
+Note that if, for example, you want to add a new plugin, you should do it against [this fork](https://github.com/henrikingo/impress.js), since upstream impress.js doesn't have a plugin API yet. Hopefully one day the progress from this fork will trickle back to upstream repository.
 
 
 ABOUT THE NAME
@@ -66,11 +107,18 @@ See the [Reference API](DOCUMENTATION.md)
 BROWSER SUPPORT
 -----------------
 
-This project supports only the major [evergreen](http://eisenbergeffect.bluespire.com/evergreen-browsers/) desktop browsers that have implemented:
+The design goal for impress.js has been to showcase awesome CSS3 features as found in modern browser versions. We also use some new DOM functionality, and specifically do not use jQuery or any other JavaScript libraries, nor our own functions, to support older browsers. In general, recent versions of Firefox and Chrome are known to work well. IE works, except there are known issues with its CSS 3D implementation, so advanced presentations won't.
+
+The typical use case for impress.js is to create presentations that you present from your own laptop, with a browser version you know works well. Some people also use impress.js successfully to embed animations or presentations in a web page, however, be aware that in this some of your visitors may not see the presentation correctly, or at all.
+
+In particular, impress.js makes use of the following JS and CSS features:
 
 * [DataSet API](http://caniuse.com/#search=dataset)
 * [ClassList API](http://caniuse.com/#search=classlist)
 * [CSS 3D Transforms](http://caniuse.com/#search=css%203d)
 * [CSS Transitions](http://caniuse.com/#search=css%20transition)
+
+COPYRIGHT AND LICENSE
+---------------------
 
 Copyright 2011-2016 Bartek Szopka - Released under the MIT [License](LICENSE)
--- vim/test/core_tests.js
+++ vim/test/core_tests.js
@@ -1,166 +1,268 @@
-(function() {
-  var registerEventListener = function( target, eventType, callback ) {
-    target.addEventListener( eventType, callback );
-    return function removeRegisteredEventListener() {
-      target.removeEventListener( eventType, callback );
+/*
+ * Copyright 2016 Henrik Ingo (@henrikingo)
+ *
+ * Released under the MIT license. See LICENSE file.
+ */
+
+QUnit.module("Core Tests");
+
+QUnit.test( "Initialize Impress.js", function( assert ) {
+  console.log("Begin init() test");
+  // Init triggers impress:init and impress:stepenter events, which we want to catch.
+  var doneInit      = assert.async();
+  var doneStepEnter = assert.async();
+  var doneSync      = assert.async();
+
+  loadIframe( "test/core_tests_presentation.html", assert, function() {
+    var iframe = document.getElementById("presentation-iframe");
+    var iframeDoc = iframe.contentDocument;
+    var iframeWin = iframe.contentWindow;
+    var root  = iframeDoc.querySelector( "div#impress" );
+
+    // Catch events triggered by init()
+    var assertInit = function( event, registeredListener ) {
+      assert.ok( true, "impress:init event triggered.");
+
+      var canvas = iframeDoc.querySelector( "div#impress > div" );
+      // delay and duration don't become set before the first transition actually happened
+      assert.equal( canvas.style.transitionDelay,
+                    "0ms",
+                    "canvas.style.transitionDelay initialized correctly" );
+      assert.equal( canvas.style.transitionDuration,
+                    "0ms",
+                    "canvas.style.transitionDuration initialized correctly" );
+
+      doneInit();
+      console.log("End init() test (async)"); 
     };
-  };
+    
+    var assertInitWrapper = function( event ) {
+      setTimeout( function() { assertInit( event ) }, 10 ); 
+    };
+    root.addEventListener( "impress:init", assertInitWrapper );
 
-  QUnit.begin(function() {
-    impress().init();
-  });
-
-  QUnit.module( "Initialization" );
-
-    QUnit.test( "Global Scope", function( assert ) {
-      assert.expect( 1 );
-      assert.ok( impress, "impress declared in global scope" );
+    root.addEventListener( "impress:stepenter", function( event ) {
+      assert.ok( true, "impress:stepenter event triggered.");
+      var step1 = iframeDoc.querySelector( "div#step-1" );
+      assert.equal( event.target, step1,
+                    event.target.id + " triggered impress:stepenter event." );
+      doneStepEnter();
     });
 
-    QUnit.test( "Multiple API instantiation", function( assert ) {
-      assert.expect( 0 );
-      impress().init();
-    });
+    // Synchronous code and assertions
+    assert.ok( iframeWin.impress, 
+               "impress declared in global scope" );
+    assert.strictEqual( iframeWin.impress().init(), undefined,
+                        "impress().init() called." );
+    assert.strictEqual( iframeWin.impress().init(), undefined,
+                        "It's ok to call impress().init() a second time, it's a no-op." );
+                      
+    // The asserts below are true immediately after impress().init() returns.
+    // Therefore we test them here, not in an event handler.
+    var notSupportedClass = iframeDoc.body.classList.contains("impress-not-supported");
+    var yesSupportedClass = iframeDoc.body.classList.contains("impress-supported");
+    if ( !_impressSupported() ) {
+      assert.ok( notSupportedClass,
+                 "body.impress-not-supported class still there." );
+      assert.ok( !yesSupportedClass,
+                 "body.impress-supported class was NOT added." );
+    } else {
+      assert.ok( !notSupportedClass,
+                 "body.impress-not-supported class was removed." );
+      assert.ok( yesSupportedClass,
+                 "body.impress-supported class was added." );
+                 
+      // To be pedantic, we run the rest of these tests inside the else 
+      // brackets as well, meaning that impress.js is tested to be supported.
+      // However, other QUnit.test() blocks than this will fail miserably if it
+      // weren't supported.
+      assert.ok( !iframeDoc.body.classList.contains("impress-disabled"),
+                 "body.impress-disabled is removed." );
+      assert.ok( iframeDoc.body.classList.contains("impress-enabled"),
+                 "body.impress-enabled is added." );
+      
+      var canvas = iframeDoc.querySelector( "div#impress > div" );
+      assert.ok( !canvas.classList.contains("step") && canvas.id === "",
+                 "Additional 'canvas' div inserted between div#impress root and steps." );
+      assert.equal( canvas.style.transform,
+                    "rotateZ(0deg) rotateY(0deg) rotateX(0deg) translate3d(1000px, 0px, 0px)",
+                    "canvas.style.transform initialized correctly" );
+      assert.equal( canvas.style.transformOrigin,
+                    "left top 0px",
+                    "canvas.style.transformOrigin initialized correctly" );
+      assert.equal( canvas.style.transformStyle,
+                    "preserve-3d",
+                    "canvas.style.transformStyle initialized correctly" );
+      assert.equal( canvas.style.transitionProperty,
+                    "all",
+                    "canvas.style.transitionProperty initialized correctly" );
+      assert.equal( canvas.style.transitionTimingFunction,
+                    "ease-in-out",
+                    "canvas.style.transitionTimingFunction initialized correctly" );
+                    
+      assert.equal( iframeDoc.documentElement.style.height,
+                    "100%",
+                    "documentElement.style.height is 100%" );
+      
+      // Steps initialization
+      var step1 = iframeDoc.querySelector( "div#step-1" );
+      assert.equal( step1.style.position,
+                    "absolute",
+                    "Step position is 'absolute'." );
 
-    QUnit.test( "Support Markup", function( assert ) {
-      assert.expect( 4 );
+      assert.ok( step1.classList.contains("active"),
+                 "Step 1 has active css class." );
+      
+    }
+    doneSync();
+    console.log("End init() test (sync)"); 
+  }); // loadIframe()
 
-      var impressNotSupported = document.body.classList.contains( "impress-not-supported" );
-      var impressSupported = document.body.classList.contains( "impress-supported" );
-      assert.ok( impressSupported, "Have class .impress-supported" );
-      assert.notOk( impressNotSupported, "Don't have class .impress-not-supported" );
+});
 
-      var impressDisabled = document.body.classList.contains( "impress-disabled" );
-      var impressEnabled = document.body.classList.contains( "impress-enabled" );
-      assert.ok( impressEnabled, "Have class .impress-enabled" );
-      assert.notOk( impressDisabled, "Don't have class .impress-disabled" );
-    });
+// Note: Here we focus on testing the core functionality of moving between
+// steps, the css classes set and unset, and events triggered.
+// TODO: more complex animations and check position, transitions, delays, etc...
+QUnit.test( "Impress Core API", function( assert ) {
+  console.log("Begin core api test");
+  var done = assert.async();
+  loadIframe( "test/core_tests_presentation.html", assert, function() {
+    initPresentation( assert, function() {
+      var iframe = document.getElementById("presentation-iframe");
+      var iframeDoc = iframe.contentDocument;
+      var iframeWin = iframe.contentWindow;
 
-    QUnit.test( "Attributes", function( assert ) {
-      assert.expect( 10 );
+      // impress.js itself uses event listeners to manipulate most CSS classes. 
+      // Wait a short while before checking, to avoid race. 
+      // (See assertStepEnterWrapper and assertStepLeaveWrapper.)
+      var wait = 5; // milliseconds
 
-      var actual, expected;
-      var root = document.querySelector( "#impress" );
-      var canvas = document.querySelector( "div#impress > div" );
+      var step1 = iframeDoc.querySelector( "div#step-1" );
+      var step2 = iframeDoc.querySelector( "div#step-2" );
+      var step3 = iframeDoc.querySelector( "div#step-3" );
+      var step4 = iframeDoc.querySelector( "div#fourth" );
+      var root  = iframeDoc.querySelector( "div#impress" );
 
-      var canvasIsNotAStep = !canvas.classList.contains("step") && canvas.id === "";
-      assert.ok( canvasIsNotAStep, "Canvas do not have step element data" );
+      // On impress:stepenter, we do some assertions on the "entered" object. 
+      // On impress:stepleave, we do some assertions on the "left" object.
+      // Finally we call next() to initialize the next transition, and it starts all over again. 
+      var i = 0;
+      var sequence = [ { left    : step1,
+                         entered : step2,
+                         next    : function(){ return iframeWin.impress().goto(2); },
+                         text    : "goto(<number>) called and returns ok (2->3)" },
+                       { left    : step2,
+                         entered : step3,
+                         next    : function(){ return iframeWin.impress().goto("fourth"); },
+                         text    : "goto(<string>) called and returns ok (3->4)" },
+                       { left    : step3,
+                         entered : step4,
+                         next    : function(){ return iframeWin.impress().next(); },
+                         text    : "next() wraps around to first step (4->1)" },
+                       { left    : step4,
+                         entered : step1,
+                         next    : function(){ return iframeWin.impress().prev(); },
+                         text    : "prev() wraps around to last step (1->4)" },
+                       { left    : step1,
+                         entered : step4,
+                         next    : function(){ return iframeWin.impress().prev(); },
+                         text    : "prev() called and returns ok (4->3)" },
+                       { left    : step4,
+                         entered : step3,
+                         next    : function(){ return iframeWin.impress().goto(0); },
+                         text    : "End of test suite, return to first step with goto(0)." },
+                       { left    : step3,
+                         entered : step1,
+                         next    : false } // false = end of sequence
+      ];
+      // When both assertStepEnter and assertStepLeave are done, we can go to next step in sequence.
+      var readyCount = 0;
+      var readyForNext = function(){
+        readyCount++;
+        if( readyCount % 2 == 0 ) {
+          if( sequence[i].next ) {
+            assert.ok( sequence[i].next(), sequence[i].text );
+            i++;
+            assertImmediately();
+          } else {
+            done();
+            console.log("End core api test");
+          }
+        }
+      };
+      
+      // Things to check on impress:stepenter event -----------------------------//
+      var assertStepEnter = function( event, registeredListener ) {
+        assert.equal( event.target, sequence[i].entered,
+                      event.target.id + " triggered impress:stepenter event." );
+        assert.ok( event.target.classList.contains("present"),
+                   event.target.id + " set present css class." );
+        assert.ok( !event.target.classList.contains("future"),
+                   event.target.id + " unset future css class." );
+        assert.ok( !event.target.classList.contains("past"),
+                   event.target.id + " unset past css class." );
+        assert.equal( "#/"+event.target.id, iframeWin.location.hash,
+                      "Hash is " + "#/"+event.target.id );
 
-      actual = canvas.style.webkitTransform || canvas.style.transform;
-      expected = "rotateZ(0deg) rotateY(0deg) rotateX(0deg) translate3d(1000px, 0px, 0px)";
-      assert.strictEqual( actual, expected, "canvas.style.transform initialized correctly" );
+        // Just by way of comparison, check transitionDuration again, in a non-init transition
+        var canvas = iframeDoc.querySelector( "div#impress > div" );
+        assert.equal( canvas.style.transitionDelay,
+                      "0ms",
+                      "canvas.style.transitionDelay set correctly" );
+        assert.equal( canvas.style.transitionDuration,
+                      "1000ms",
+                      "canvas.style.transitionDuration set correctly" );
 
-      // Normalize result for IE 11 and Safari.
-      actual = canvas.style.webkitTransformOrigin || canvas.style.transformOrigin;
-      expected = "left top 0px";
 
-      if ( actual === "left top" || actual === "0% 0%" ) {
-        actual = expected;
-      }
-      assert.strictEqual( actual, expected, "canvas.style.transformOrigin initialized correctly" );
+        readyForNext();
+      };
+      
+      var assertStepEnterWrapper = function( event ) {
+        setTimeout( function() { assertStepEnter( event ) }, wait ); 
+      };
+      root.addEventListener( "impress:stepenter", assertStepEnterWrapper );
 
-      actual = canvas.style.webkitTransformStyle || canvas.style.transformStyle;
-      expected = "preserve-3d";
-      assert.strictEqual( actual, expected, "canvas.style.transformStyle initialized correctly" );
 
-      actual = canvas.style.transitionDelay;
-      expected = "0ms";
-      assert.strictEqual( actual, expected, "canvas.style.transitionDelay initialized correctly" );
+      // Things to check on impress:stepleave event -----------------------------//
+      var assertStepLeave = function( event, registeredListener ) {
+        assert.equal( event.target, sequence[i].left,
+                      event.target.id + " triggered impress:stepleave event." );
+        assert.ok( !event.target.classList.contains("present"),
+                   event.target.id + " unset present css class." );
+        assert.ok( !event.target.classList.contains("future"),
+                   event.target.id + " unset future css class." );
+        assert.ok( event.target.classList.contains("past"),
+                   event.target.id + " set past css class." );
+        readyForNext();
+      };
 
-      actual = canvas.style.transitionDuration;
-      expected = "0ms";
-      assert.strictEqual( actual, expected, "canvas.style.transitionDuration initialized correctly" );
+      var assertStepLeaveWrapper = function( event ) {
+        setTimeout( function() { assertStepLeave( event ) }, wait );
+      };
+      root.addEventListener( "impress:stepleave", assertStepLeaveWrapper );
+      
+      // Things to check immediately after impress().goto() ---------------------------//
+      var assertImmediately = function(){
+        assert.ok( sequence[i].entered.classList.contains("active"),
+                   sequence[i].entered.id + " set active css class." );
+        assert.ok( !sequence[i].left.classList.contains("active"),
+                   sequence[i].left.id + " unset active css class." );      
+      };
 
-      actual = canvas.style.transitionProperty;
-      expected = "all";
-      assert.strictEqual( actual, expected, "canvas.style.transitionProperty initialized correctly" );
 
-      actual = canvas.style.transitionTimingFunction;
-      expected = "ease-in-out";
-      assert.strictEqual( actual, expected, "canvas.style.transitionTimingFunction initialized correctly" );
+      // Done with setup. Start testing! -----------------------------------------------//
+      // Do no-op tests first, then trigger the sequence of transitions we setup above. //
 
-      actual = root.style.perspective;
-      expected = "";
-      assert.notStrictEqual( actual, expected, "root.style.perspective should be set explicitly for IE 11" );
+      assert.strictEqual( iframeWin.impress().goto(iframeDoc.querySelector( "div#impress" )),
+                        false,
+                        "goto() to a non-step element fails, as it should." );
+      assert.strictEqual( iframeWin.impress().goto(),
+                        false,
+                        "goto(<nothing>) fails, as it should." );
 
-      actual = document.documentElement.style.height;
-      expected = "100%";
-      assert.strictEqual( actual, expected, "documentElement.style.height is 100%" );
-    });
-
-    QUnit.test( "Steps", function( assert ) {
-      assert.expect( 2 );
-
-      var actual, expected;
-      var step1 = document.querySelector( "div#step-1" );
-
-      actual = step1.style.position;
-      expected = "absolute";
-      assert.strictEqual( actual, expected, "Step position is 'absolute'" );
-
-      assert.ok( step1.classList.contains( "active" ), "Step 1 has active css class." );
-    });
-
-  QUnit.module( "Core API" );
-
-    QUnit.test( ".next()", function( assert ) {
-      assert.expect( 2 );
-
-      var root = document.querySelector( "div#impress" );
-      var step1 = document.querySelector( "div#step-1" );
-      var step2 = document.querySelector( "div#step-2" );
-      var done = assert.async();
-
-      impress().next();
-
-      assert.ok( step2.classList.contains( "active" ), step2.id + " add active css class." );
-      assert.notOk( step1.classList.contains( "active" ), step1.id + " remove active css class." );
-
-      // Reset to original state
-      var removeStepEnterEvent = registerEventListener( root, "impress:stepenter",  function() {
-        removeStepEnterEvent();
-        done();
-      });
-      impress().goto( step1 );
-    });
-
-    QUnit.test( "impress:stepenter event", function( assert ) {
-      assert.expect( 4 );
-
-      var actual, expected;
-      var root = document.querySelector( "div#impress" );
-      var step1 = document.querySelector( "div#step-1" );
-      var step2 = document.querySelector( "div#step-2" );
-      var done = assert.async();
-
-      var removeTestEvent = registerEventListener( root, "impress:stepenter",  function( event ) {
-        actual = event.target;
-        expected = step2;
-        assert.strictEqual( actual, expected, "Triggered event for the second step" );
-
-        assert.ok( step2.classList.contains( "present" ), event.target.id + " add present css class" );
-        assert.notOk( step2.classList.contains( "future" ), event.target.id + " remove future css class" );
-        assert.notOk( step2.classList.contains( "past" ), event.target.id + " remove active css class." );
-
-        removeTestEvent();
-
-        // Reset to original state
-        var removeCleanupEvent = registerEventListener( root, "impress:stepenter",  function() {
-          removeCleanupEvent();
-          done();
-        });
-
-        impress().goto( step1 );
-      });
-
-      impress().next();
-    });
-
-  QUnit.done(function( details ) {
-    // Impress.js will set the hash part of the url, we want to unset it when finished
-    // Otherwise a refresh of browser page would not start tests from the last step step
-    window.location.hash = "";
-    // Add back vertical scrollbar so we can read results if there were failures.
-    document.body.style.overflow = 'auto';
-  });
-}());
+      // This starts executing the sequence above
+      assert.ok( iframeWin.impress().next(),
+                 "next() called and returns ok (1->2)");
+    }); // initPresentation()
+  }); // loadIframe()
+});
